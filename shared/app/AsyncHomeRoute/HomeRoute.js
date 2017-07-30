ecbe825b83a6ba8035640d8b1d8203fc6383d8f8
Tirta Nugraha <betotrr@gmail.com>
2017-07-30 22:48:11 +0700

    Basic Route transition animation

--- shared/app/AsyncHomeRoute/HomeRoute.js
+++ shared/app/AsyncHomeRoute/HomeRoute.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 
 import config from './../../../config';
 
-function HomeRoute() {
-  return (
-    <div>
-      <Helmet>
-        <title>Home</title>
-      </Helmet>
+class HomeRoute extends Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Home</title>
+        </Helmet>
 
-      <h2>{config('welcomeMessage')}</h2>
+        <h2>{config('welcomeMessage')}</h2>
 
-      <p>
-        This starter kit contains all the build tooling and configuration you
-        need to kick off your next universal React project, whilst containing a
-        minimal project set up allowing you to make your own architecture
-        decisions (Redux/Mobx etc).
-      </p>
-    </div>
-  );
+        <p>
+          This starter kit contains all the build tooling and configuration you
+          need to kick off your next universal React project, whilst containing a
+          minimal project set up allowing you to make your own architecture
+          decisions (Redux/Mobx etc).
+        </p>
+      </div>
+    );
+  }
 }
 
 export default HomeRoute;
